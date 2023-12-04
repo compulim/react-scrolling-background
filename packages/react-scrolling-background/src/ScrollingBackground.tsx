cc53a40e00a6c9d62e6696752c2ce960ec6eb1ac
William Wong <compulim@hotmail.com>
2023-12-04 00:05:47 -0800

    Clean up

--- packages/react-scrolling-background/src/ScrollingBackground.tsx
+++ packages/react-scrolling-background/src/ScrollingBackground.tsx
@@ -6,16 +6,24 @@ import mathRandom from 'math-random';
 import createClassName from './private/createClassName';
 
 type Props = {
-  /** CSS class to apply to the background element. */
+  /** CSS class to apply to the background element.*/
   backgroundClassName?: string | undefined;
 
-  /** Background image of type [CSS <image>](https://developer.mozilla.org/en-US/docs/Web/CSS/image). */
+  /**
+   * Background image of type [CSS <image>](https://developer.mozilla.org/en-US/docs/Web/CSS/image).
+   *
+   * Can also specify via CSS custom property `--react-scrolling-background__background-image` in the element selected by `backgroundClassName`.
+   */
   backgroundImage?: string | undefined;
 
   /** CSS class to apply to the background container element. */
   className?: string | undefined;
 
-  /** Durations (in milliseconds) to scroll the background. Defaults to 10 minutes. */
+  /**
+   * Durations (in milliseconds) to scroll the background. Defaults to 10 minutes.
+   *
+   * Can also specify via CSS custom property `--react-scrolling-background__duration` in the element selected by `backgroundClassName`.
+   */
   duration?: number | undefined;
 
   /** Nonce for injecting style sheet. */
@@ -25,33 +33,13 @@ type Props = {
    * Speed of the scrolling defined by number of full rotations done within the duration. Defaults to 3, meaning 3 full rotations in 10 minutes.
    *
    * Note: Speed will change depends on the container width. Narrower container will have its background scroll slower than wider container.
+   *
+   * Can also specify via CSS custom property `--react-scrolling-background__speed` in the element selected by `backgroundClassName`.
    */
   speed?: number | undefined;
 };
 
 const CSS_KEY = 'css-rsb';
-
-// CSS.registerProperty({
-//   inherits: false,
-//   // initialValue: 'unset',
-//   name: '--react-scrolling-background__background-image',
-//   // syntax: '<image>'
-//   syntax: '*'
-// });
-
-// CSS.registerProperty({
-//   inherits: false,
-//   initialValue: '600s',
-//   name: '--react-scrolling-background__duration',
-//   syntax: '<time>'
-// });
-
-// CSS.registerProperty({
-//   inherits: false,
-//   initialValue: '3',
-//   name: '--react-scrolling-background__speed',
-//   syntax: '<number>'
-// });
 
 const ScrollingBackground = memo(
   ({ backgroundClassName, backgroundImage, className, duration = 600_000, nonce, speed = 3 }: Props) => {
