2622cf86a50ac2fe43e967d8a54989266e5f1cae
William Wong <compulim@hotmail.com>
2023-12-18 04:59:30 -0800

    Use inject-meta-tag package

--- packages/react-scrolling-background/src/index.ts
+++ packages/react-scrolling-background/src/index.ts
@@ -1,4 +1,16 @@
-import './private/injectMeta';
+import { npm } from 'inject-meta-tag';
+
 import ScrollingBackground from './ScrollingBackground';
+
+declare global {
+  const process: {
+    env: {
+      npm_package_name: string;
+      npm_package_version: string;
+    };
+  };
+}
+
+npm(process.env.npm_package_name, { version: process.env.npm_package_version });
 
 export { ScrollingBackground };
