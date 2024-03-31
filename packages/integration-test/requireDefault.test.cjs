3c73db0ca2d75bad828e9ce81ae262041df0bcde
William Wong <compulim@hotmail.com>
2024-03-31 15:46:37 -0700

    Fix inject tag

--- packages/integration-test/requireDefault.test.cjs
+++ packages/integration-test/requireDefault.test.cjs
@@ -6,3 +6,9 @@ const { ScrollingBackground, TestIds } = require('react-scrolling-background');
 
 test('simple scenario', () =>
   expect(create(<ScrollingBackground />).root.findByProps({ 'data-testid': TestIds.root })).toBeTruthy());
+
+test('importing the package should inject package metadata', () =>
+  expect(document.head.querySelector('meta[name="npm:react-scrolling-background"]')).toHaveProperty(
+    'content',
+    'version=0.0.0-0'
+  ));
