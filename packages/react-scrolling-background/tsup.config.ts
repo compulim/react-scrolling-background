5f78b7ab41b4bbc48ca3262013269d4c3f846c27
@compulim <@compulim>
2025-12-24 01:56:56 +0000

    Bump scaffold

--- packages/react-scrolling-background/tsup.config.ts
+++ packages/react-scrolling-background/tsup.config.ts
@@ -2,15 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    define: {
-      'ReactScrollingBackground.npm_package_name': JSON.stringify(process.env.npm_package_name || ''),
-      'ReactScrollingBackground.npm_package_version': JSON.stringify(process.env.npm_package_version || '')
-    },
     dts: true,
     entry: {
       'react-scrolling-background': './src/index.ts'
     },
     format: ['cjs', 'esm'],
-    sourcemap: true
+    sourcemap: true,
+    target: 'esnext'
   }
 ]);
